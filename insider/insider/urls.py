471dfa135ad99f523b7c82efb1e6ad3cd9650c73
Vishnu Suresh <vishnu@example.com>
2026-07-08 13:27:19 +0530

    Update URL routing

--- insider/insider/urls.py
+++ insider/insider/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for insider project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 from myapp import views
+ 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.index),
+    path("login", views.login_view),
+    path("signout", views.signout),
+ 
+    # Registration
+    path("register_employee", views.register_employee),
+ 
+    # Admin
+    path("admin_home", views.admin_home),
+    path("admin_view_employees", views.admin_view_employees),
+    path("admin_employee_action", views.admin_employee_action),
 ]
