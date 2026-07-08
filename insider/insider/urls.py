f44f480dd866d91cd98f7670e5200938853f3cbd
Vishnu Suresh <vishnu@example.com>
2026-07-08 14:50:49 +0530

    Fix URL configurations with trailing slashes and admin import

--- insider/insider/urls.py
+++ insider/insider/urls.py
@@ -1,16 +1,17 @@
+from django.contrib import admin
 from django.urls import path
 from myapp import views
  
 urlpatterns = [
     path("", views.index),
-    path("login", views.login_view),
-    path("signout", views.signout),
+    path("login/", views.login_view),
+    path("signout/", views.signout),
  
     # Registration
-    path("register_employee", views.register_employee),
+    path("register_employee/", views.register_employee),
  
     # Admin
-    path("admin_home", views.admin_home),
-    path("admin_view_employees", views.admin_view_employees),
-    path("admin_employee_action", views.admin_employee_action),
+    path("admin_home/", views.admin_home),
+    path("admin_view_employees/", views.admin_view_employees),
+    path("admin_employee_action/", views.admin_employee_action),
 ]
