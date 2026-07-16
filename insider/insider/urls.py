e93877df358d11fe996a95f5cbf6f9f45cf2e445
Vishnu Suresh <vishnu@example.com>
2026-07-16 16:35:16 +0530

    Update URL routes in insider/insider/urls.py

--- insider/insider/urls.py
+++ insider/insider/urls.py
@@ -1,18 +1,25 @@
-from django.contrib import admin
 from django.urls import path
 from myapp import views
- 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("", views.index),
     path("login/", views.login_view),
     path("signout/", views.signout),
- 
+
     # Registration
     path("register_employee/", views.register_employee),
- 
+
     # Admin
     path("admin_home/", views.admin_home),
     path("admin_view_employees/", views.admin_view_employees),
     path("admin_employee_action/", views.admin_employee_action),
+    path("admin_view_activities/", views.admin_view_activities),
+    path("admin_view_alerts/", views.admin_view_alerts),
+    path("admin_alert_action/", views.admin_alert_action),
+    path("admin_retrain_model/", views.admin_retrain_model),
+
+    # Employee
+    path("employee_home/", views.employee_home),
+    path("log_activit/y", views.log_activity),
+    path("my_activity/", views.my_activity),
 ]
